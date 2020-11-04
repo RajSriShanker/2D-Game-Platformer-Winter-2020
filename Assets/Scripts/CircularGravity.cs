967c95c1dfd6397a3e0805457c3b655ba7666c32
Raj SS <raj.ss166@gmail.com>
2020-11-03 20:43:06 -0500

    Bare Bones
    Just setting up things, dont have anything great with this commit.

---
+++ Assets/Scripts/CircularGravity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularGravity : MonoBehaviour
+{
+    public float massOfEarth;
+    public Transform centerOfEarth;
+    public float gravity;
+    
+    float massOfPlayer;
+    float distance;
+    float forceValue;
+
+    Vector3 forceDirection;
+    Rigidbody2D playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        massOfPlayer = playerRB.mass;
+        distance = Vector3.Distance(centerOfEarth.position, transform.position);
+        forceValue = gravity * (massOfEarth * massOfPlayer) / (distance * distance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forceDirection = (centerOfEarth.position - transform.position).normalized;
+        playerRB.AddForce(forceValue * forceDirection);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float jumpForce;
+
+    Rigidbody2D playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            playerRB.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision)
+        {
+            Debug.Log(collision);
+            GameObject.Find("Player").GetComponent<CircularGravity>().centerOfEarth = collision.gameObject.transform;
+        }
+    }
+}
